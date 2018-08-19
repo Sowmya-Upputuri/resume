3664f608e908076ad759a071e48b59b33aeda81f
upputurisowmya16NN1A05G5 <42430768+upputurisowmya16NN1A05G5@users.noreply.github.com>
2018-08-19 11:20:10 +0530

    Add files via upload

---
+++ mail3.js
@@ -0,0 +1,50 @@
+{
+	"basicdetails":{
+		"image":"Penguins.jpg",
+		"name":"sowmya",
+		"email":"upputurisowmya@gmail.com",
+		"phone":"7093075511",
+		"address":"kannavarithota,guntur(27-9-1)"
+
+	},
+	"education":[
+		{
+			"cource":"B.tech(C.S.E)",
+			"college":"Vignan nirula",
+			"percentage":"76%" 
+		},
+{
+			"cource":"intermediate(MPC)",
+			"college":"NRI college",
+			"percentage":"89%" 
+		},
+{
+			"cource":"SSC",
+			"college":"ST.Josephs",
+			"percentage":"93%" 
+		}
+	],
+	"skills":[
+		{
+			"name":"Operating Systems",
+			"value":"Windows,Linux,MAC"
+
+		},
+		{
+			"name":"Programming Languages",
+			"value":"C,C++,Java,Python"
+
+		}
+	],
+
+	"carrier":{
+		"ca":"I am using my knowledge and skills towards the growth of the oraganisation and i obey the oraganisation rules and take my responsiblity end ofmy working days"
+
+},
+"achievement":[
+	" I got fist rank in btech",
+ " I am placed in mnc",
+	"I got 1st prize in ppt"
+
+
+]
